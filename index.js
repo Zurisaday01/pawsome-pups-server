f4bc08944ff3144dc07875f40f53c3ba6d34d4d0
Zurisaday Espadas <zuryespadas@Zurisadays-MacBook-Air.local>
2023-08-03 17:55:27 -0600

    cors

--- index.js
+++ index.js
@@ -70,7 +70,6 @@ app.use(function (req, res, next) {
 	res.header('Access-Control-Allow-Methods','GET, POST, PATCH, PUT, DELETE, OPTIONS');
 	res.header('Access-Control-Allow-Headers','Origin, X-Requested-With, Content-Type, Accept, Authorization, set-cookie');
 	res.header('Access-Control-Allow-Credentials', true);	
-	res.
 
 	next();
 });
