aac75ffbcba328a9ff378d332db37c19bb536091
Zurisaday Espadas <zuryespadas@Zurisadays-MacBook-Air.local>
2023-08-03 11:06:22 -0600

    🔵

--- index.js
+++ index.js
@@ -67,7 +67,7 @@ app.use(express.urlencoded({ extended: true }));
 app.use(function (req, res, next) {
 	res.header("Access-Control-Allow-Origin", "*");
 	res.header('Access-Control-Allow-Methods','GET, POST, PATCH, PUT, DELETE, OPTIONS');
-	res.header('Access-Control-Allow-Headers','Origin, X-Requested-With, Content-Type, Accept');
+	res.header('Access-Control-Allow-Headers','Origin, X-Requested-With, Content-Type, Accept, Authorization');
 	res.header('Access-Control-Allow-Credentials', true);	
 
 	next();
