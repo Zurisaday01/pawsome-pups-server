ac375994eea12912b62651b6c09d7b5d15091d18
Zurisaday Espadas <zuryespadas@Zurisadays-MacBook-Air.local>
2023-08-03 10:46:11 -0600

    🟤

--- index.js
+++ index.js
@@ -35,7 +35,10 @@ const app = express();
 app.enable('trust proxy');
 
 // Implement CORS
-app.options('*', cors({origin: true, credentials: true}));
+app.use(cors({
+	origin: true,
+	credentials: true,
+}));
 
 // Set security HTTP headers
 app.use(helmet());
@@ -64,6 +67,7 @@ app.use(express.urlencoded({ extended: true }));
 app.use(function (req, res, next) {
 	res.header('Access-Control-Allow-Origin: *');
 	res.header('Access-Control-Allow-Methods: GET, POST, PATCH, PUT, DELETE, OPTIONS');
+	res.header('Access-Control-Allow-Headers','Origin, X-Requested-With, Content-Type, Accept');
 	res.header('Access-Control-Allow-Credentials', true);
 
 	next();
