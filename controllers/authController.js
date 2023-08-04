d7692806695e6b00b6830ca55690d36b685211a7
Zurisaday Espadas <zuryespadas@Zurisadays-MacBook-Air.local>
2023-08-04 10:44:57 -0600

    cookie logged out

--- controllers/authController.js
+++ controllers/authController.js
@@ -71,6 +71,7 @@ export const logout = (req, res) => {
 	res.cookie('jwt', 'loggedout', {
 		expires: new Date(Date.now() + 10 * 1000),
 		httpOnly: true,
+		sameSite: 'none', secure: true
 	});
 	res.status(200).json({ status: 'success' });
 };
